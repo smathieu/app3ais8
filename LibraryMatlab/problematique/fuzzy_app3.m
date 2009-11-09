1a1b14f5e3d74e0aafa692490b07cdc8dacdd53f
unknown <Admin@.(none)>
2009-11-09 16:49:55 -0500

    Nicer version of fuzzy logic

--- LibraryMatlab/problematique/fuzzy_app3.m
+++ LibraryMatlab/problematique/fuzzy_app3.m
@@ -4,47 +4,80 @@ function fuzzy_app3
     clear all
     clc
     
-    fuzzy_logic_fis = readfis('fuzzy.fis');
-    falls = fuzzy_get_preprocessed_data(get_falls);
-    non_falls = fuzzy_get_preprocessed_data(get_non_falls);
+    fuzzy_logic_fis = readfis('stupid_fuzzy.fis');
+%     falls = fuzzy_get_preprocessed_data(get_falls);
+%     non_falls = fuzzy_get_preprocessed_data(get_non_falls);
+    falls = get_falls;
+    non_falls = get_non_falls;
 
-    % Optimize get_peak_count
-%     for i = 1:7
-%     data = falls(i).Sensor2;
-%     figure
-%     plot(data)
-%     
-%     data = non_falls(i).Sensor2;
-%     figure
-%     plot(data)
-% %     get_peak_count(data);
-%     end
-    
-    falls_count = 0;
-    non_falls_count = 0;
-    values = non_falls;
+
+ for k = 1:15
+    f_falls_count = 0;
+    f_non_falls_count = 0;
+    values = falls;
     for i = 1:length(values)
         data = values(i);
-%         figure
-%         plot(1:length(data.Sensor2), data.Sensor2, 'r', ...
-%             1:length(data.Sensor3), data.Sensor3, 'b', ...
-%             1:length(data.Sensor5), data.Sensor5, 'g', ...
-%             1:length(data.Sensor6), data.Sensor6, 'y');
-        inputs = [ ...
-            get_peak_count(data.Sensor2), ...
-            get_peak_count(data.Sensor3), ...
-            get_peak_count(data.Sensor5), ...
-            get_peak_count(data.Sensor6) ...
-            ]
-        output = evalfis( inputs, fuzzy_logic_fis)
         
-        if( output >= 0.5 )
-            falls_count = falls_count + 1;
+        a4 = downsample_signal(data.Sensor4, 100, 2000);
+        a5 = downsample_signal(data.Sensor5, 100, 2000);
+        a6 = downsample_signal(data.Sensor6, 100, 2000);
+        inputs = [abs(abs(max(a6))-abs(min(a6))), ...
+                  abs(abs(max(a5))-abs(min(a5))), ...
+                  abs(abs(max(a4))-abs(min(a4))) ...
+                  ];
+%         inputs = [ ...
+%             get_peak_count(data.Sensor2), ...
+%             get_peak_count(data.Sensor3), ...
+%             get_peak_count(data.Sensor5), ...
+%             get_peak_count(data.Sensor6) ...
+%             ]
+        output = evalfis( inputs, fuzzy_logic_fis);
+        
+        if( output >= (0.35 + k/100) )
+%         if( output >= (0.44) )
+            f_falls_count = f_falls_count + 1;
         else
-            non_falls_count = non_falls_count + 1;
+            f_non_falls_count = f_non_falls_count + 1;
         end
     end
     
-    total = falls_count + non_falls_count;
-    fprintf('Falls : %d/%d => %d\n', falls_count, total, round(falls_count/total*100))
-    fprintf('Non falls : %d/%d => %d\n', non_falls_count, total, round(non_falls_count/total*100))
+    threshold = 0.35 + k/100
+    f_total = f_falls_count + f_non_falls_count;
+    fprintf('F  : Falls : %d/%d => %d\n', f_falls_count, f_total, round(f_falls_count/f_total*100))
+    fprintf('F  : Non falls : %d/%d => %d\n', f_non_falls_count, f_total, round(f_non_falls_count/f_total*100))
+    
+    nf_falls_count = 0;
+    nf_non_falls_count = 0;
+    values = non_falls;
+    for i = 1:length(values)
+        data = values(i);
+        
+        a4 = downsample_signal(data.Sensor4, 100, 2000);
+        a5 = downsample_signal(data.Sensor5, 100, 2000);
+        a6 = downsample_signal(data.Sensor6, 100, 2000);
+        inputs = [abs(abs(max(a6))-abs(min(a6))), ...
+                  abs(abs(max(a5))-abs(min(a5))), ...
+                  abs(abs(max(a4))-abs(min(a4))) ...
+                  ];
+%         inputs = [ ...
+%             get_peak_count(data.Sensor2), ...
+%             get_peak_count(data.Sensor3), ...
+%             get_peak_count(data.Sensor5), ...
+%             get_peak_count(data.Sensor6) ...
+%             ]
+        output = evalfis( inputs, fuzzy_logic_fis);
+        
+        if( output >= (0.35 + k/100) )
+%         if( output >= (0.44) )
+            nf_falls_count = nf_falls_count + 1;
+        else
+            nf_non_falls_count = nf_non_falls_count + 1;
+        end
+    end
+    
+    nf_total = nf_falls_count + nf_non_falls_count;
+    fprintf('NF : Falls : %d/%d => %d\n', nf_falls_count, nf_total, round(nf_falls_count/nf_total*100))
+    fprintf('NF : Non falls : %d/%d => %d\n', nf_non_falls_count, nf_total, round(nf_non_falls_count/nf_total*100))
+    
+    moy = (round(f_falls_count/f_total*100) + round(nf_non_falls_count/nf_total*100))/2
+end
